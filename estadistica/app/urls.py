c35693721c5650dadf2594555f084dd964cac1a9
Kevin Guadamuz <kevin.guadamuz@yahoo.com>
2023-08-15 01:09:09 -0600

    Mas avances

--- estadistica/app/urls.py
+++ estadistica/app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import DatosExcel
+from .views import DatosExcel,  FrecuenciaExcel
 
 urlpatterns = [
      path("", views.index, name="index"),
      path('Datos/', DatosExcel.as_view(), name='Excel-Datos'),
+     path('frecuencia/', FrecuenciaExcel.as_view(), name='Frecuencia'),
 ]
 
--- estadistica/app/views.py
+++ estadistica/app/views.py
@@ -6,11 +6,31 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import math
+import numpy as np
+
+arreglo = []
 
 def index(request):
     return render(request, "index.html")
 
 # Api
+class FrecuenciaExcel(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        datos1 = []
+        datos = request.data
+        print(datos)
+        for i in datos:
+            datos1.append(float(i))
+
+        # Calcular la frecuencia
+        frecuencia = np.histogram(arreglo, bins=datos1)[0]
+        print(frecuencia)
+        
+        return Response("A",status=status.HTTP_201_CREATED)
+
+
 class DatosExcel(APIView):
     permission_classes = [AllowAny]
 
@@ -21,7 +41,7 @@ class DatosExcel(APIView):
         datos = request.data
         flattened_data = [float(item) for sublist in datos for item in sublist]
         print(flattened_data)
-
+        arreglo.append(flattened_data)
         # Numero de datos
         N_datos = len(flattened_data)
 
@@ -32,15 +52,17 @@ class DatosExcel(APIView):
         N_menor = min(flattened_data)
 
         # Rango
-        rango = N_mayor - N_menor
+        rango = round(N_mayor - N_menor,2)
+        rango_rendondeado = math.ceil(N_mayor - N_menor)
 
         # N° deinterva
         N_intervalos = 1+3.322 * math.log10(N_datos)
-
+        N_redondeado =   math.ceil(N_intervalos)
+        print(f"{rango_rendondeado} /{N_redondeado}")
         # Amplitud
-        Amplitud = rango/N_intervalos
-
-
+        Amplitud = rango_rendondeado/N_redondeado
+        print(Amplitud)
+      
         return Response([N_datos,  N_mayor, N_menor
                          , rango, N_intervalos, Amplitud], status=status.HTTP_201_CREATED)
     
