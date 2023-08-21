721cc799081c3341080bbf7de7d78928cce03625
Kevin Guadamuz <kevin.guadamuz@yahoo.com>
2023-08-21 15:14:15 -0600

    Se finalizo

--- estadistica/app/urls.py
+++ estadistica/app/urls.py
@@ -6,8 +6,9 @@ from .views import guia2
 
 urlpatterns = [
      path("", views.index, name="index"),
-    path('guia2/', guia2, name= "guia2"),
-     path('Datos/', DatosExcel.as_view(), name='Excel-Datos'),
+     path("principal/", views.IndexView, name="principal"),
+     path('guia2/', guia2, name= "guia2"),
+
      path('frecuencia/', FrecuenciaExcel.as_view(), name='Frecuencia'),
 ]
 
--- estadistica/app/views.py
+++ estadistica/app/views.py
@@ -7,14 +7,19 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from statistics import mean, median, mode, variance, stdev
 from numpy import percentile, array, repeat
-import math
+from math import floor, ceil, log10
+import pandas as pd
 import numpy as np
 
 N_datos = 0
 arreglo = []
 
+def principal(request):
+    return render(request, "principal.html")
+
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, "principal.html")
 
 def guia2(request):
     if request.method == 'POST':
@@ -105,6 +110,135 @@ def guia2(request):
 
 
 # Api
+
+# Funcion para contar numeros en un rango (Frecuencia absoluta)
+
+def freqabs(lista, linferior, lsuperior):
+    # Filtrar elementos en la lista que coincidan en los intervalos Y contarlos
+    return len([x for x in lista if x >= linferior and x <= lsuperior])
+
+# Funcion para acumular freq absolutas
+
+def freqabsacum(lista):
+    acumulado = 0
+    # Recorriendo la lista de freq absolutas
+    for i in lista:
+        # Sumando la freq anterior
+        acumulado = acumulado + i
+    # Retornando el valor 
+    return acumulado
+
+
+def freqrel(lista,total): #Funcion que calcula la frecuencia relativa / frecuencia relativa acumulada
+
+    for i in lista:#Recorriendo la lista de freq absolutas / absoluta acumulada
+        freqactual = i / total #Calculando la frecuencia relativa
+    # Retornando el valor
+    return freqactual
+
+def freqpor(lista): #Funcion para calcular la freq porcentual / freq porcentual acumulada
+
+    for i in lista: #Recorriendo la lista de freq relativas / freq relativa acumulada
+        freqactual = i * 100 #Calculando la frecuencia porcentual
+    # Retornando el valor
+    return freqactual
+
+
+def IndexView(request):
+    if request.method == 'POST':
+        input_data = request.POST["tabla_datos"]
+        
+        if not input_data:
+            return Response({'message': 'No hay datos'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        table = pd.DataFrame(columns=[ #Se crea un dataframe(estructura bidimensional) para asignar los datos
+            'Numero',
+            'Clase',
+            'Marcasdeclase',
+            'Frecuenciaabsoluta',
+            'Frecuenciaabsolutaacumulada',
+            'Frecuenciarelativa',
+            'Frecuenciarelativaacumulada',
+        ])
+
+        #Asignamos las listas, estas estaran al inicio del POST para limpiar sus datos cada refresco de pagina
+        data = [] #Este contendrá los datos proporcionados por el usuario
+        fabs  = [] #Frecuencia absoluta
+        fabsacum  = [] #Frecuencia absoluta acumulada
+        frel = [] #Frecuencia relativa
+        frelacum = [] #Frecuencia relativa acumulada
+        marcas = [] #marcas de clase
+
+
+        data = [float(value) for value in input_data.split()]
+
+        #Formulas utilizadas
+        Total = len(data) #Este toma el total de datos que hay en la lista
+        MaxValue = max(data) #Obtiene el dato mas alto
+        MinValue = min(data) #Obtiene el dato mas bajo
+        r =  max(data)-min(data) #Rango
+        linferior = floor(MinValue) #Redondear al entero mas cercano para tomar el limite inferior
+        intervalos = 1 + (3.322 * log10(Total)) #Formula para sacar los intervalos
+        k = round(intervalos) #Redondeamos el dato
+        lon = r / k #Longitud o amplitud
+        
+        #Formulas de la guia2
+        mean = np.mean(data) #media aritmetica
+        median = np.median(data) #mediana
+        variance = np.var(data, ddof=1)  #Usar ddof=1 para calcular la varianza muestral
+        std_deviation = np.std(data, ddof=1)  #Usar ddof=1 para calcular la desviación estándar muestral
+
+        q1 = np.percentile(data, 25) #primer cuartil
+        q3 = np.percentile(data, 75) #tercer cuartil
+        interquartile_range = q3 - q1
+        #data.sort()  # Ordenar en forma creciente
+        sorted_data = sorted(data)  #Obtener una nueva lista ordenada
+        
+        for i in range(1, round(intervalos)+1): #Ciclo que recorre los intervalos
+
+            lsuperior = ceil(linferior + lon) #Calculando el limite superior y redondeandolo a su próximo más cercano
+            marcas.append((linferior + lsuperior) / 2) #Añadiendo a la lista las marcas de clase
+            fabs.append(freqabs (data, linferior, lsuperior)) #Añadiendo a la lista las frecuencias absolutas 
+            fabsacum.append(freqabsacum(fabs)) #Añadiendo a la lista Las frecuencias absolutas acumuladas
+            frel.append(freqrel(fabs,Total)) #Añadiendo a la lista Las frecuencias relativas 
+            frelacum.append(freqrel(fabsacum,Total)) #Añadiendo a la lista las frecuencias relativas acumuladas
+
+            table.loc[i] = [i,f"[{linferior}, {lsuperior})", (linferior + lsuperior) / 2, freqabs(data, linferior, lsuperior), freqabsacum(fabs), freqrel(fabs,Total), freqrel(fabsacum,Total)] #Añadiendole valores a la tabla
+
+            linferior = lsuperior#Modificando en valor del límite inferior en cada iteración
+
+
+        #Se divivio en dos para mejor orden
+        data_dict = {
+            "Media Aritmética": mean,
+            "Mediana": median,
+            "Varianza Muestral": variance,
+            "Desviación Estándar Muestral": std_deviation,
+            "Primer Cuartil": q1,
+            "Tercer Cuartil": q3,
+            "Rango Intercuartil": interquartile_range,
+        }
+
+        data_dict2 = {
+            "Total de datos": Total,
+            "Valor mínimo": min(data),
+            "Valor máximo": max(data),
+            "Rango": r,
+        }      
+
+        context = {
+            'table': table,
+            'list': data_dict,
+            'list2': data_dict2,
+            'sort_list': sorted_data,
+        }
+        print(context)
+        
+        return render(request, "tablas.html", context)
+    return render(request, "principal.html")
+
+
+
 class FrecuenciaExcel(APIView):
     permission_classes = [AllowAny]
 
@@ -149,11 +283,11 @@ class DatosExcel(APIView):
 
         # Rango
         rango = round(N_mayor - N_menor,2)
-        rango_rendondeado = math.ceil(N_mayor - N_menor)
+        rango_rendondeado = ceil(N_mayor - N_menor)
 
         # N° deinterva
-        N_intervalos = 1+3.322 * math.log10(N_datos)
-        N_redondeado =   math.ceil(N_intervalos)
+        N_intervalos = 1+3.322 * log10(N_datos)
+        N_redondeado =   ceil(N_intervalos)
        # print(f"{rango_rendondeado} /{N_redondeado}")
         # Amplitud
         Amplitud = rango_rendondeado/N_redondeado
